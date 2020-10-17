1046efbdcde6b94c27050ae50d53b3475a6efaca
Mariusz Szleszynski <mariusz.szleszynski@outlook.com>
2020-10-17 20:02:39 +0200

    Create correlation mausemove vs backgroundColor

---
+++ main.js
@@ -0,0 +1,18 @@
+const h1 = document.querySelector('h1')
+
+document.body.addEventListener('mousemove', function (event) {
+  // console.log(event.clientX, event.clientY);
+  const x = event.clientX;
+  const y = event.clientY;
+  const width = window.innerWidth;
+  const height = window.innerHeight;
+
+  h1.textContent = `${event.clientX}, ${event.clientY}`
+
+  const red = x / width * 100;
+  const green = y / height * 100;
+  const blue = ((x / width * 100) + (y / height * 100)) / 2;
+
+  document.body.style.backgroundColor = `rgb(${red}%, ${green}%, ${blue}%)`
+
+})
